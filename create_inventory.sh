41ed90fe4658bc2478b296da9b57ccf3f4e97e59
Peter Ehrenberg <pe@dipe.de>
2024-02-29 12:07:04 +0100

    Change: Authority: If it exists, only the 'Developer ID Application' is output as the publisher. If not, the 3 summarised authority lines as before Change: Output adapted so that the csv can be opened directly with Excel. New: Guessed App Name: Very simple check whether .app is in the path. If yes, this part is used as Guessed App Name. New: Team identifier added as column again. New: Rows are only written if the TeamIdentifier is not "not set". New: Display of the execution duration, because interesting 🙂 New: Delete the TEMPDIR that was created. Add more content to readme.md

--- create_inventory.sh
+++ create_inventory.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -e "Working...\nThis can take between 3 and 30 minutes. Please be patient."
+SECONDS=0
+
+echo -e "Working...\nThis can take between 3 and 90+ minutes. Please be patient and don't close this window."
 echo "You don't have to wait. You can continue your work and switch back here later."
 
 TEMP=`basename -s .sh $0`
@@ -28,18 +30,29 @@ find / \( -xdev -type d -name "*.app" -o -type f -perm +111 -print0 \) -o \( \( 
                 $vanilla_mac_book{$path} = 1;
             }
 
-            print "Type,App Name,Publisher Details\n"; 
+            binmode(STDOUT, ":encoding(UTF-8)");
+            print "\"Type\";\"Publisher Details\";\"TeamIdentifier\";\"Guessed App Name\";\"Path\"\n";
         } 
-
         /^Executable=(.*)/ and do { 
-            print "$type,$exec,\"" . join(";", @auths) . "\"\n" if $exec && !exists $vanilla_mac_book{$exec}; 
-            $exec = $1; @auths = (); $type = ""; next; 
+            if ($exec) {
+                my $appName = "";
+                if ($exec =~ /\/([^\/]+)\.app/) {
+                    $appName = $1;  # Extrahiert den Namen der Anwendung
+                }else{
+                    $appName = "? check path";
+                }
+                my $authority_to_print = $devIdApp ? $devIdApp : join(";", @auths);
+                print "\"$type\";\"$authority_to_print\";\"$team\";\"$appName\";\"$exec\"\n" if $exec && !exists $vanilla_mac_book{$exec} && $team ne "not set"; 
+            }
+           $exec = $1; @auths = (); $type = ""; $devIdApp = ""; $team = ""; next; 
         }; 
-        /^Authority=(.*)/ and do { 
-            (my $auth = $1) =~ s/,/./g; # Ersetzt alle Kommas durch Punkte
-            push @auths, $auth; 
-            next; 
-        }; 
+        /^Authority=Developer ID Application: (.*)/ and do {
+            $devIdApp = $1 unless $devIdApp; 
+        };
+        /^Authority=(.*)/ and do {
+            (my $auth = $1) =~ s/,/./g;
+            push @auths, $auth unless $devIdApp;
+        };
         /Format=bundle/ and do { 
             $type="Bundle";
         }; 
@@ -51,9 +64,19 @@ find / \( -xdev -type d -name "*.app" -o -type f -perm +111 -print0 \) -o \( \( 
         }; 
         /Format=generic/ and do { 
             $type="Generic";
-        }; 
+        };
+        /^TeamIdentifier=(.*)/ and do {
+            $team = $1;
+        };
         END { 
-            print "$type,$exec,\"" . join(";", @auths) . "\"\n" if $exec && !exists $vanilla_mac_book{$exec}; 
+            my $appName = ""; 
+            if ($exec =~ /\/([^\/]+)\.app/) {
+                $appName = $1;  # Extrahiert den Namen der Anwendung
+            }else{
+                $appName = "? check path";
+            }
+            my $authority_to_print = $devIdApp ? $devIdApp : join(";", @auths);
+            print "\"$type\";\"$authority_to_print\";\"$team\";\"$appName\";\"$exec\"\n" if $exec && !exists $vanilla_mac_book{$exec} && $team ne "not set"; 
         }
     ' >square_tisax_inventory.csv
 
@@ -63,6 +86,7 @@ echo -e "\nCreating zip archive:"
 ZIPFILE=${DIRNAME}.zip 
 zip -r ${ZIPFILE} ${DIRNAME}
 mv ${ZIPFILE} ~/.
+rm -r ${TEMPDIR}
 echo -e "done.\n"
-
+echo "The execution of the script took $SECONDS seconds."
 echo "You will now find a file named ${ZIPFILE} in your user directory ${USER}."
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
+# Software Inventory Creation
+We are subject to the safety regulations of our clients. The automotive industry requires its suppliers to fulfil a security standard called TISAX. TISAX prescribes in great detail how clients' confidential data must be handled.
+
+TISAX has now introduced a new policy according to which not just any software may be installed on the computer, but software must be on an allow list.
+
+Apart from the future processes for this, we want to carry out a software inventory to initially get our currently used software on the allow list.
+
+We have written a program that searches for all executable files on your disk and reads their signature, if available. These signatures are saved together with the respective path. 
+
+We collect and aggregate this data in a second step, using the essence of this to supplement the list of allowed software. 
+
+If you want to know what data is in the zip archive, you can open it. You will find two files. A log file with potential error messages from the programme run and a .csv file that can be opened with Excel. It contains the paths for executables found and details from their signature (Authority and TeamIdentified), which are required for the allow list. The program also attempts to guess the name of the software, which should make the subsequent process easier.
+
 # Usage
 
 Open terminal.app and copy paste this line. Answer execustion confimation with yes.
@@ -6,14 +19,27 @@ Open terminal.app and copy paste this line. Answer execustion confimation with y
 sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/dipe/square-software-inventory/main/create_inventory.sh)"
 ```
 
+Dialogues with access requests may occur while the software is running. 
+
+![Access request](zugriffsanfrage.png)
+
+You should allow access to folders containing programs. You can deny access to folders containing personal documents and photos. However, the data will not be read.
+
+If you run the programme several times, you may be asked at the end whether the previous zip file may be overwritten by the new one. You should answer yes to this question;
+
+```
+override rw-r--r--  root/wheel for /Users/peter.ehrenberg/./AMATX36VVY9JX.zip?
+y
+```
+
 # Example
 ```
 $ sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/dipe/square-software-inventory/main/create_inventory.sh)"
 Confirm Execution: This Sudo Command requires admin rights. Are you sure you wish to proceed?
 Yes/No: yes
 Working...
-This can take between 3 and 30 minutes. Please be patient. You don't have to wait. 
-You can continue your work and switch back here later.
+This can take between 3 and 90+ minutes. Please be patient.
+You don't have to wait. You can continue your work and switch back here later.
 
 Creating zip archive:
   adding: XXATX36AFY9DX/ (stored 0%)
